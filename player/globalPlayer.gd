e06e9036470ecd45946882a19109b60c43185e94
spookyoo <melvincruz07@gmail.com>
2024-12-04 16:15:30 -0600

    Instrument database

--- player/globalPlayer.gd
+++ player/globalPlayer.gd
@@ -3,7 +3,14 @@ extends Node
 var instrument = ""
 var player
 
-
+func changeInstrument(instrument, rarity):
+	match instrument:
+		"lute":
+			
+		"drum":
+			
+		"recorder":
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
