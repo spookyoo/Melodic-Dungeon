3bd76f7e6b8cf2a90c5c58fd88867f576a8b72e9
spookyoo <melvincruz07@gmail.com>
2024-12-04 15:07:20 -0600

    Added HUD elements
    - score
    - inventory
    - timer
    - text font

--- player/hud/mapHud.gd
+++ player/hud/mapHud.gd
@@ -16,24 +16,27 @@ func _ready():
 	#getChildAt(3,3).modulate.a = 1.0
 
 func updateMap(x,z):
-	print(completed)
 	for child in get_children():
 		child.modulate.a = 0.0
-		child.get_node("inner").color = Color(0.553, 0.435, 0.687)
+		child.get_node("inner").color = Color(0.584, 0.468, 0.718)
 	
 	var grid = getGrid(x,z)
-	print(grid)
 	for row in range(grid.size()):
 		for column in range(grid[row].size()):
 			if (grid[row][column] > 0):
-				if (grid[row][column] == 1):
-					pass
+				if (grid[row][column] == 1 || grid[row][column] == 3):
+					if getAdjacent(row,column,grid):
+						getChildAt(row,column).modulate.a = 1.0
+						if grid[row][column] == 1:
+							getChildAt(row,column).get_node("inner").color = Color(0.338, 0.247, 0.438)
+						else:
+							getChildAt(row,column).get_node("inner").color = Color(0.747, 0.365, 0.683)
 				else:
-					
-				#print(str(row) + " " + str(column))
 					getChildAt(row,column).modulate.a = 1.0
+
 	getChildAt(2,2).modulate.a = 1.0
 	getChildAt(2,2).get_node("inner").color = Color(0.831, 0.745, 0.894)
+	getAdjacent(2,2,grid)
 	return
 
 #HELPER FUNCTIONS
@@ -62,8 +65,23 @@ func getGrid(x,z):
 			else:
 				gridRow.append(0)
 		newGrid.append(gridRow)
+	newGrid[2][2] = 2
 	newGrid.reverse()
 	return newGrid
+
+func getAdjacent(x,z,grid):
+	if z > 0 && grid[x][z-1] == 2:
+		return true
+	if z < 4 && grid[x][z+1] == 2:
+		return true
+	if x > 0 && grid[x-1][z] == 2:
+		return true
+	if x < 4 && grid[x+1][z] == 2:
+		return true
+	return false
+
+func inBounds(x, z):
+	return (x > -1 && x < 5) && (z > -1 && z < 5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
